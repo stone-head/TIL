8bad35e27c70c5bc929f606c274dad680a1e76f1
stone-head <jh.kim+stonehead@core-a.org>
2018-07-18 22:46:37 +0900

    Dollar 의 equal 비교를 할 때, amount 값을 가지고 동등성을 비교함

--- book/tdd/src/main/com/stonehead/tdd/Dollar.kt
+++ book/tdd/src/main/com/stonehead/tdd/Dollar.kt
@@ -5,7 +5,6 @@ class Dollar(var amount: Int) {
     fun time(multiplier: Int): Dollar = Dollar(multiplier * amount)
 
     override fun equals(other: Any?): Boolean {
-        // return super.equals(other)
-        return true
+        return amount == (other as Dollar).amount
     }
 }
