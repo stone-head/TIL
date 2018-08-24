e793cb5be12fb46e4fe86a386bd708f00b20b1c4
stone-head <jh.kim+stonehead@core-a.org>
2018-08-24 23:50:05 +0900

    tdd python 시작

---
+++ book/tdd_python/first.py
@@ -0,0 +1,10 @@
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+    def testMethod(self):
+        pass
+
+test = WasRun("testMethod")
+print test.wasRun
+test.testMethod()
+print test.wasRun
